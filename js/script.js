1762bc0ff5b95546f2b7cca9967b4b9d83977f37
sayemahmed <sayem.ahmed@welldev.io>
2024-06-25 10:21:44 +0600

    Delete Todo task feature implemented.

--- js/script.js
+++ js/script.js
@@ -4,17 +4,47 @@ const unoderedList = document.getElementById("tasks");
 
 let tasks = [];
 const addTaskHandler = ()=>{
-    const taskTitle = taskInput.value.trim();
+    const taskTitle = taskInput.value.trim()+" ";
     if(taskTitle){
-        tasks.unshift(taskTitle);
-        const tasksList = document.createElement("li");
-        tasksList.textContent = taskTitle;
-        unoderedList.prepend(tasksList);
-        taskInput.value = "";
+        createTask(taskTitle);
     }
     else
         alert("Please provide valid Task.");
 };
+
+
+const createTask = (taskTitle)=>{
+    const task = {
+        id: new Date().getTime(),
+        title: taskTitle
+    };
+    tasks.unshift(task);
+    renderTasks();
+
+    taskInput.value = "";
+};
+
+const deleteHandler = (taskId) => {
+    tasks = tasks.filter((task) => task.id !== taskId);
+    renderTasks();
+};
+
+const renderTasks = () => {
+    unoderedList.innerHTML = '';
+
+    tasks.forEach((task) => {
+        const tasksList = document.createElement("li");
+        tasksList.textContent = task.title;
+
+        const deleteButton = document.createElement("button");
+              deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", () => deleteHandler(task.id));
+
+        tasksList.appendChild(deleteButton);
+        unoderedList.appendChild(tasksList);
+    });
+};
+
 
 
 addButton.addEventListener("click",addTaskHandler);
