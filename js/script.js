56a1a46dad81ceb0e6ea8ddee19f398aec1f2f27
sayemahmed <sayem.ahmed@welldev.io>
2024-06-26 14:31:44 +0600

    Executed prettier effects to format files.

--- js/script.js
+++ js/script.js
@@ -3,48 +3,43 @@ const addButton = document.getElementById("btn-add");
 const unoderedList = document.getElementById("tasks");
 
 let tasks = [];
-const addTaskHandler = ()=>{
-    const taskTitle = taskInput.value.trim()+" ";
-    if(taskTitle){
-        createTask(taskTitle);
-    }
-    else
-        alert("Please provide valid Task.");
+const addTaskHandler = () => {
+  const taskTitle = taskInput.value.trim() + " ";
+  if (taskTitle) {
+    createTask(taskTitle);
+  } else alert("Please provide valid Task.");
 };
 
+const createTask = (taskTitle) => {
+  const task = {
+    id: new Date().getTime(),
+    title: taskTitle,
+  };
+  tasks.unshift(task);
+  renderTasks();
 
-const createTask = (taskTitle)=>{
-    const task = {
-        id: new Date().getTime(),
-        title: taskTitle
-    };
-    tasks.unshift(task);
-    renderTasks();
-
-    taskInput.value = "";
+  taskInput.value = "";
 };
 
 const deleteHandler = (taskId) => {
-    tasks = tasks.filter((task) => task.id !== taskId);
-    renderTasks();
+  tasks = tasks.filter((task) => task.id !== taskId);
+  renderTasks();
 };
 
 const renderTasks = () => {
-    unoderedList.innerHTML = '';
+  unoderedList.innerHTML = "";
 
-    tasks.forEach((task) => {
-        const tasksList = document.createElement("li");
-        tasksList.textContent = task.title;
+  tasks.forEach((task) => {
+    const tasksList = document.createElement("li");
+    tasksList.textContent = task.title;
 
-        const deleteButton = document.createElement("button");
-              deleteButton.innerText = "Delete";
-        deleteButton.addEventListener("click", () => deleteHandler(task.id));
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => deleteHandler(task.id));
 
-        tasksList.appendChild(deleteButton);
-        unoderedList.appendChild(tasksList);
-    });
+    tasksList.appendChild(deleteButton);
+    unoderedList.appendChild(tasksList);
+  });
 };
 
-
-
-addButton.addEventListener("click",addTaskHandler);
+addButton.addEventListener("click", addTaskHandler);
