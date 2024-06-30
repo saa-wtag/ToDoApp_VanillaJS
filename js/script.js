31253f2e1b3f8870af7229cc8ee10d529e3346f5
sayemahmed <sayem.ahmed@welldev.io>
2024-06-30 14:37:35 +0600

    Some inconsistency resolved.

--- js/script.js
+++ js/script.js
@@ -6,16 +6,43 @@ let tasks = [];
 const addButtonHandler = () => {
     const taskTitle = $taskInput.value.trim();
     if (taskTitle) {
-        tasks.unshift(taskTitle);
-        const $tasksList = document.createElement("li");
-        $tasksList.textContent = taskTitle;
-        $taskList.prepend($tasksList);
-        $taskInput.value = "";
+        createTask(taskTitle);
         showToastMessage("Task added successfully!");
         $addButton.disabled = true;
     }
 };
 
+const deleteHandler = (taskId) => {
+  tasks = tasks.filter((task) => task.id !== taskId);
+  renderTasks();
+};
+
+const createTask = (taskTitle) => {
+  const task = {
+    id: new Date().getTime(),
+    title: taskTitle,
+  };
+  tasks.unshift(task);
+  renderTasks();
+
+  $taskInput.value = "";
+};
+
+const renderTasks = () => {
+  $taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const tasksList = document.createElement("li");
+    tasksList.textContent = task.title;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => deleteHandler(task.id));
+
+    tasksList.appendChild(deleteButton);
+    $taskList.appendChild(tasksList);
+  });
+};
 
 $taskInput.addEventListener("input", () => {
     if ($taskInput.value.trim()) {
