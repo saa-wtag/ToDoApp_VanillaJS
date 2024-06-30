1fc0828f03c734075d23272c17a02e2bbdb0c440
sayemahmed <sayem.ahmed@welldev.io>
2024-06-30 14:29:45 +0600

    Created "utilities.js" file to separate utility functions from the "script.js" file.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
-import {$taskInput, $addButton, $taskList, $toaster } from "./elements.js";
+import { $taskInput, $addButton, $taskList } from "./elements.js";
+import { showToastMessage } from "./utilities.js";
 
 let tasks = [];
 
 const addButtonHandler = () => {
-  const taskTitle = $taskInput.value.trim();
-  if (taskTitle) {
-    tasks.unshift(taskTitle);
-    const $tasksList = document.createElement("li");
-    $tasksList.textContent = taskTitle;
-    $taskList.prepend($tasksList);
-    $taskInput.value = "";
-    showToastMessage("Task added successfully!");
-    $addButton.disabled = true;
-  }
-};
-
-const showToastMessage = (message) => {
-  $toaster.textContent = message;
-  $toaster.hidden = false;
-  setTimeout(() => {
-    $toaster.hidden = true;
-  }, 3000);
+    const taskTitle = $taskInput.value.trim();
+    if (taskTitle) {
+        tasks.unshift(taskTitle);
+        const $tasksList = document.createElement("li");
+        $tasksList.textContent = taskTitle;
+        $taskList.prepend($tasksList);
+        $taskInput.value = "";
+        showToastMessage("Task added successfully!");
+        $addButton.disabled = true;
+    }
 };
 
 $taskInput.addEventListener("input", () => {
-  if ($taskInput.value.trim()) {
-    $addButton.disabled = false;
-  } else {
-    $addButton.disabled = true;
-  }
+    if ($taskInput.value.trim()) {
+        $addButton.disabled = false;
+    } else {
+        $addButton.disabled = true;
+    }
 });
 
 $addButton.addEventListener("click", addButtonHandler);
