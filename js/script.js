3c97ee80a05ef6d13044746da3a33d76442356da
sayemahmed <sayem.ahmed@welldev.io>
2024-07-01 14:32:41 +0600

    Modified the codes by maintaining modularity concept

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 import { $taskInput, $addButton, $taskList } from "./elements.js";
-import { showToastMessage, sanitizeInput } from "./utilities.js";
+import {
+  showToastMessage,
+  sanitizeInput,
+  createElement,
+  formatDate,
+} from "./utilities.js";
 
 let tasks = [];
 
 const addButtonHandler = () => {
   const taskTitle = sanitizeInput($taskInput.value);
   if (taskTitle) {
-    tasks.unshift(taskTitle);
-    const $tasksList = document.createElement("li");
-    $tasksList.textContent = taskTitle;
-    $taskList.prepend($tasksList);
-    $taskInput.value = "";
+    createTask(taskTitle);
     showToastMessage("Task added successfully!");
     $addButton.disabled = true;
-  }
-  else
-    showToastMessage("Please provide a valid title!");
+  } else showToastMessage("Please provide a valid title!");
+};
+
+const createTask = (taskTitle) => {
+  const task = {
+    id: new Date().getTime(),
+    title: taskTitle,
+    createdAt: formatDate(new Date()),
+  };
+  tasks.unshift(task);
+  renderTasks();
+
+  $taskInput.value = "";
+};
+
+const renderTasks = () => {
+  $taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const texts = [task.title, " Created at: " + task.createdAt];
+    const $tasksList = createElement(texts, "li");
+    $taskList.appendChild($tasksList);
+  });
 };
 
 $taskInput.addEventListener("input", () => {
--- js/utilities.js
+++ js/utilities.js
@@ -12,3 +12,18 @@ export const sanitizeInput = (value) => {
   const reg = /[&<>"'/`]/gi;
   return value.replace(reg, "").trim();
 };
+
+export const createElement = (text, type, onClick) => {
+  const $element = document.createElement(type);
+  $element.innerText = text;
+  if (onClick) $element.addEventListener("click", onClick);
+  return $element;
+};
+
+export const formatDate = (now) => {
+  const day = now.getDate().toString().padStart(2, "0");
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
+  const year = now.getFullYear().toString().slice(-2);
+
+  return `${day},${month},${year}`;
+};
